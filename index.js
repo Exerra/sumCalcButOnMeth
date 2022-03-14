7470553053c319804033e6f54f6450260487747d
semvis123 <semvisscher10@gmail.com>
2022-03-14 15:02:28 +0100

    add number parser and number factory

--- index.js
+++ index.js
@@ -4,15 +4,76 @@
 	You can only edit everything *inside* of myFunction
  */
 function myFunction(a, b) {
+	class number {
+		constructor(value) {
+			this.value = value;
+		}
+		getValue() {
+			return this.value;
+		}
+	}
+
+	class numberFactory {
+		createNewNumberWithValue(value) {
+			return new number(value);
+		}
+	}
+
 	const getValue = async (a, b) => {
+		const parseNumber = (numberString) => {
+			digits = {
+				'0': 0,
+				'1': 1,
+				'2': 2,
+				'3': 3,
+				'4': 4,
+				'5': 5,
+				'6': 6,
+				'7': 7,
+				'8': 8,
+				'9': 9,
+			}
+			nDigits = numberString.split('')
+			num = 0
+			isNegative = false
+			hasDecimal = false
+			decimalDigits = 0
+			nDigits.forEach((digit) => {
+				if (digits[digit] !== undefined) {
+					num *= 10
+					num += digits[digit]
+					if (hasDecimal) {
+						decimalDigits += 1
+					}
+				} else if (digit === '.') {
+					hasDecimal = true
+				} else if (digit === '-') {
+					isNegative = true
+				}
+			})
+			for (let i = 0; i < decimalDigits; i++) {
+				num /= 10
+			}
+			if (isNegative) {
+				num = -num
+			}
+			return num
+		}
+
 		const getNumber = async (number) => {
 			const encode = (number) => {
 				let arr = []
 
 				for (let i = 0; i < number.toString().length; i++) {
 					let x = number.toString().charAt(i)
-					x = x * 4
-					arr.push(+x)
+					if (x === '.') {
+						arr.push('.')
+					} else if (x === '-') {
+						arr.push('-')
+					} else {
+						x = x * 4
+						arr.push(+x)
+					}
 				}
 
 				return arr
@@ -22,15 +83,20 @@ function myFunction(a, b) {
 				let str = ""
 
 				arr.forEach(s => {
-					s = s / 4
-					str += s.toString()
+					if (s === '.') {
+						str += '.'
+					} else if (s === '-') {
+						str += '-'
+					} else {
+						s = s / 4
+						str += s.toString()
+					}
 				})
-
-				return parseInt(str)
+				return parseNumber(str)
 			}
 
 			const checkType = (number) => {
-				return typeof (number + 1)
+				return ['.', '-'].includes(number) ? 'number' : typeof (number + 1)
 			}
 
 			let res = encode(number)
@@ -53,17 +119,25 @@ function myFunction(a, b) {
 		const as = await (await getA(a, b)).toString()
 		const bs = await (await getB(a, b)).toString()
 
-		const ai = parseInt(as)
-		const bi = parseInt(bs)
+		const ai = parseNumber(as)
+		const bi = parseNumber(bs)
+		
+		const numFact = new numberFactory()
+		const an = numFact.createNewNumberWithValue(ai)
+		const bn = numFact.createNewNumberWithValue(bi)
 
-		const add = (x, y) => {
-			if (y == 0)
-				return x;
+		const add = async (x, y) => {
+			const numFact = new numberFactory()
+			const zero = numFact.createNewNumberWithValue(0)
+			const one = numFact.createNewNumberWithValue(1)
+
+			if (y.getValue() == zero.getValue())
+				return x.getValue();
 			else
-				return myFunction(x ^ y, (x & y) << 1);
+				return myFunction(x.getValue() ^ y.getValue(), (x.getValue() & y.getValue()) << one.getValue());
 		}
 
-		let sum = await add(ai, bi)
+		let sum = await add(an, bn)
 
 		return sum
 	}
